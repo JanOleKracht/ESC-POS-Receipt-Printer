184fa3a64c08f5d722511ec0cf254f6623b193d0
Jan-Ole Kracht <jk.kracht@gmail.com>
2026-02-16 11:02:09 +0100

    Added xUnitTests

---
+++ Test/Core.Bonprinter.Tests/CalculationTests.cs
@@ -0,0 +1,102 @@
+namespace Core.Bonprinter.Tests
+{
+    public class CalculationTests
+    {
+        [Fact]
+        public void CalculateBruttoGesamt_ShouldCalculateCorrectly()
+        {
+            // Arrange
+            var beleg = new Beleg
+            {
+                BelegPositionen = new List<BelegPosition>
+                {
+                    new BelegPosition {Menge=2,Brutto=20m, MwstZeichen="A" },
+                    new BelegPosition {Menge=1,Brutto=10.5m, MwstZeichen="B" },
+                }
+            };
+
+            // Act
+            beleg.BerechneSumme();
+
+            // Assert
+            Assert.Equal(30.5m, beleg.BruttoGesamt);
+        }
+
+        [Fact]
+        public void CalculateNettoGesamt_ShouldCalculateCorrectly()
+        {
+            // Arrange
+
+            var beleg = new Beleg
+            {
+                BelegPositionen = new List<BelegPosition>
+              {
+                  new BelegPosition {Menge=2,Brutto=20m, MwstZeichen="A", Netto=16.81m },
+                  new BelegPosition {Menge=1,Brutto=10.5m, MwstZeichen="B", Netto=9.82m },
+                }
+            };
+
+            // Act
+            beleg.BerechneSumme();
+
+            // Assert
+            Assert.Equal(26.62m, beleg.NettoGesamt);
+        }
+
+        [Fact]
+        public void CalculateMwstBetragA_ShouldCalculateCorrectly()
+        {
+            // Arrange
+            var beleg = new Beleg
+            {
+                BelegPositionen = new List<BelegPosition>
+                {
+                    new BelegPosition {Menge=2,Brutto=20m, MwstZeichen="A", Netto=16.81m },
+                    new BelegPosition {Menge=1,Brutto=10.5m, MwstZeichen="B", Netto=9.82m },
+                }
+            };
+            // Act
+            beleg.BerechneSumme();
+            // Assert
+            Assert.Equal(3.19m, beleg.MwstBetragA);
+        }
+
+        [Fact]
+        public void CalculateMwstBetragB_ShouldCalculateCorrectly()
+        {
+            // Arrange
+
+            var beleg = new Beleg
+            {
+                BelegPositionen = new List<BelegPosition>
+                {
+                    new BelegPosition { Menge = 2, Brutto = 20m, MwstZeichen = "A", Netto = 16.81m },
+                    new BelegPosition { Menge = 1, Brutto = 10.5m, MwstZeichen = "B", Netto = 9.82m },
+                }
+            };
+
+            // Act
+            beleg.BerechneSumme();
+
+            // Assert
+            Assert.Equal(0.69m, beleg.MwstBetragB);
+        }
+
+        [Fact]
+        public void CaculateSumme_PositionIsNull_EverythingSouldBeNull()
+        {
+            //Arrange
+            var beleg = new Beleg
+            {
+                BelegPositionen = new List<BelegPosition>()
+            };
+            //Act
+            beleg.BerechneSumme();
+            //Assert
+            Assert.Equal(0m, beleg.BruttoGesamt);
+            Assert.Equal(0m, beleg.NettoGesamt);
+            Assert.Equal(0m, beleg.MwstBetragA);
+            Assert.Equal(0m, beleg.MwstBetragB);
+        }
+    }
+}
